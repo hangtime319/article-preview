4e94a7a573ecf5013fd20330718471b122d9dda8
hangtime319 <hangtime31@hotmail.com>
2024-05-31 00:44:05 -0300

    Atualizando arquivos e inserindo nova imagem

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const 
+const btn_share = document.querySelector(".share");
+const btn_shareMedia = document.querySelector(".social-media picture img")
+const user = document.querySelector(".content-user")
+const socialMedia = document.querySelector(".social-media");
+
+function shareSocialMedia() {
+    socialMedia.classList.toggle("display-none");
+}
+
+btn_share.addEventListener("click", shareSocialMedia);
+btn_shareMedia.addEventListener("click", shareSocialMedia);
+
