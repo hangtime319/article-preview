ef2de735c6cf564a86a72e2724bb225da4191f8b
hangtime319 <hangtime31@hotmail.com>
2024-06-01 13:49:06 -0300

    Atualizando script.js

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ const user = document.querySelector(".content-user")
 const socialMedia = document.querySelector(".social-media");
 
 function shareSocialMedia() {
+    var screenSize = window.screen.width;
     socialMedia.classList.toggle("display-none");
+    
+    if (screenSize > 376) {
+        btn_shareMedia.classList.add("display-none");
+    }
 }
 
 btn_share.addEventListener("click", shareSocialMedia);
